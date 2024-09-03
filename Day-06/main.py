e3457c68bd2d136ca2daf62c8c8d3ecdc75b75d7
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-09-03 11:28:29 -0400

    chore: Update main.py with secure login functionality

---
+++ Day-06/main.py
@@ -0,0 +1,10 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+password = input("Password> ")
+
+if username == "mark" and password == "password":
+    print("Welcome Mark! You have successfully logged in.")
+elif username == "suzanne":
+    print("Welcome Suzanne! You have successfully logged in.")
+else:
+    print("Sorry, you are unable to log in. Please try again.")
