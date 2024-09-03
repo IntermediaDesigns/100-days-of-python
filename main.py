4ee9c16622aef9a2a53c37e5c3da087b20402dfc
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-09-03 15:50:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+movie = input("What is your favorite movie? ")
+
+if movie.lower() == "the godfather":
+  director = input("Who directed The Godfather? ")
+  if director.lower() == "francis ford coppola":
+    print("Correct! You're a true superfan of The Godfather!")
+  else: 
+    print("Sorry, that's not correct. Better luck next time!")
+
+elif movie.lower() == "pulp fiction":
+  actor = input("Who plays Vincent Vega in Pulp Fiction? ")
+  if actor.lower() == "john travolta":
+    print("Correct! You're a true superfan of Pulp Fiction!")
+  else:
+    print("Sorry, that's not correct. Better luck next time!")
+
+elif movie.lower() == "the dark knight":
+  joker = input("Who plays the Joker in The Dark Knight? ")
+  if joker.lower() == "heath ledger":
+    print("Correct! You're a true superfan of The Dark Knight!")
+  else:
+    print("Sorry, that's not correct. Better luck next time!")
