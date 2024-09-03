b48eb290db77314c075653cfc7916e58ea14c03c
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-09-03 11:03:20 -0400

    chore: Update main.py with user input and print statements

---
+++ Day-04/main.py
@@ -0,0 +1,56 @@
+print("Welcome to the mad libs simulator. I am going to ask you a bunch of questions.")
+print()
+adjective1 = input("Give an adjective (a word that describes a person or thing): ")
+place = input("Give a place: ")
+adjective2 = input("Give an adjective (a word that describes a person or thing): ")
+animal = input("Give an animal: ")
+adjective3 = input("Give an adjective (a word that describes a person or thing): ")
+object1 = input("Give an object: ")
+verbIng = input("Give a verb ending in -ing: ")
+
+exclamation = input("Give an exclamation: ")
+adjective4 = input("Give an adjective (a word that describes a person or thing): ")
+object2 = input("Give an object: ")
+
+adjective5 = input("Give an adjective (a word that describes a person or thing): ")
+profession = input("Give a profession: ")
+place2 = input("Give a place: ")
+
+print()
+print(
+    "In the",
+    adjective1,
+    "kingdom of",
+    place,
+    ", there lived a",
+    adjective2,
+    animal + ". One day, the",
+    animal,
+    "found a",
+    adjective3,
+    object1,
+    "while",
+    verbIng + ".",
+)
+print()
+print(
+    exclamation + "!",
+    "said the",
+    animal + ",",
+    "I have never seen a",
+    adjective4,
+    object2,
+    "before!",
+)
+print()
+print(
+    "And from that day forward, the",
+    animal,
+    "was known as the",
+    adjective5,
+    profession,
+    "of",
+    place2 + ".",
+)
+print()
+print("The End")
