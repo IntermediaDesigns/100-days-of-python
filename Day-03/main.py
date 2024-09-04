f257510e3145635986a9e80a31a65c5a8e64bd2d
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-09-04 14:18:56 -0400

    Delete Day-03 directory

--- Day-03/main.py
+++
@@ -1,9 +0,0 @@
-food = input("Name a food: ")
-plant = input("Name a type of plant: ")
-cooking = input("Name a method of cooking: ")
-burned = input("What word describes burned food? ")
-item = input("Name a household item: ")
-
-print()
-print("MENU")
-print(cooking, food, "with", burned, plant, "on a bed of", item)
