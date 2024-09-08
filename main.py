6192a19d3a2b5ee6c0789f655f0c6a3b24c54aee
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2024-09-08 16:18:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+print(" 🌟 Life Organizer 🌟 ")
+print()
+
+try:
+  with open("todo.txt", "r") as f:
+    toDoList = eval(f.read())
+except FileNotFoundError:
+  toDoList = []
+
+def printList():
+  print()
+  for item in toDoList:
+    print(item)
+  print()
+
+
+while True:
+  menu = input("Do you want to add, view, edit or remove a to do? ")
+  print()
+  if menu == "add":
+    item = input("What is the task? ")
+    print()
+    due = input("When is it due by? ")
+    print()
+    priority = input("What is the priority? ")
+    print()
+    row = [item, due, priority]
+    toDoList.append(row)
+    print("Thanks, this task has been added.")
+    print()
+  elif menu == "view":
+    view = input("Do you want to view all or view priority? ")
+    print()
+    if view == "all":
+      print("Here is your to do list: ")
+      print()
+      printList()
+      print()
+    elif view == "priority":
+      priority = input("What priority? ")
+      print()
+      for row in toDoList:
+        if priority in row:
+          for item in row:
+            print(item, end=" | ")
+          print()
+      print()
+  elif menu == "edit":
+    edit = input("What do you want to edit? ")
+    print()
+    for row in toDoList:
+      if edit in row:
+        toDoList.remove(row)
+        item = input("What is the task? ")
+        print()
+        due = input("When is it due by? ")
+        print()
+        priority = input("What is the priority? ")
+        print()
+        row = [item, due, priority]
+        toDoList.append(row)
+        print("Thanks, this task has been edited.")
+        print()
+  elif menu == "remove":
+    remove = input("What do you want to remove? ")
+    print()
+    for row in toDoList:
+      if remove in row:
+        toDoList.remove(row)
+        print("Thanks, this task has been removed.")
+        print()
+  else:
+    print("Invalid input")
+    print()
+
+  with open("todo.txt", "w") as f:
+    f.write(str(toDoList))
+
+  quit = input("Do you want to see the menu again or quit?  ")
+  print()
+  if quit == "quit" or quit == "no":
+    exit()
+  else:
+    continue
